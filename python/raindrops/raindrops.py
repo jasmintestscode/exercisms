a0f9d0cc64b3c3fe322108c6e8bcd8eebdb9bef8
Jasmin Smith <jasminsmith@Jasmins-MBP.attlocal.net>
2020-12-02 14:43:38 -0600

    first commit

---
+++ python/raindrops/raindrops.py
@@ -0,0 +1,12 @@
+def convert(number):
+    rain = ""
+    if number % 3 == 0:
+        rain = rain + "Pling"
+    if number % 5 == 0:
+        rain = rain + "Plang"
+    if number % 7 == 0:
+        rain = rain + "Plong"
+    if rain == "":
+        rain = str(number)
+    return rain
+
